d0f3018ce0e3bcaa267fc3dd09a64f3a794783b1
liumcb <liumcb@163.com>
2020-04-02 14:19:54 +0800

    修改src内容

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
 import React from 'react';
-const ReactDemo = () => (
- <h1>这是我的第一个 react npm 组件</h1>
-);
-export default ReactDemo;
+
+class Demo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            width:100,
+            height: 100,
+            background: 'red'
+        }
+    }
+    widthHandle(e){
+        this.setState({
+            width: e.target.value
+        });
+    }
+    heightHandle(e){
+        this.setState({
+            height: e.target.value
+        });
+    }
+    backgroundHandle(e){
+        this.setState({
+            background: e.target.value
+        });
+    }
+    render(){
+        const {width,height ,background} = this.state;
+        const styleParam = {
+            width,
+            height,
+            background,
+            marginTop:'20px'
+        }
+        return(
+            <div>
+                <div>宽：<input onChange={(e) =>this.widthHandle(e)} /></div>
+                <div>高：<input onChange={(e) =>this.heightHandle(e)} /></div>
+                <div>背景色：<input onChange={(e) =>this.backgroundHandle(e)} /></div>
+                <div style={styleParam}>我是小框框</div>
+            </div>
+        )
+    }
+}
+
+export default Demo;
